af31fd5ca580a394c92a4ae8cbab92e3da5d7e15
Sakina Kantawala <sakinakantawala1602@gmail.com>
2026-07-14 08:53:39 +0400

    add autopilot loop to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from drone_state import DroneState
 from pid_controller import PIDController
 from kalman_filter import KalmanFilter
-
+import random
 
 '''if __name__ == "__main__":    #checks if the file is being excecuted directly or is imported and excecuted in another file
     drone1 = DroneState(0, 0, 0, 0, 0, 0,'Drone A')
@@ -45,11 +45,10 @@ print(kf.estimate, kf.variance)
 # Step 2: predict again, then update with another noisy measurement
 kf.predict(velocity=1, dt=1)
 kf.update(measurement=2.2, measurement_variance=0.5)
-print(kf.estimate, kf.variance)'''
+print(kf.estimate, kf.variance)
 
-import random
-from drone_state import DroneState
-from kalman_filter import KalmanFilter
+
+
 
 drone = DroneState(0, 0, 0, 0, 0, 1, "TrueDrone")  # vz=1, climbing steadily
 kf = KalmanFilter(estimate=0, variance=1, process_noise=0.01)
@@ -64,4 +63,35 @@ for step in range(20):
     noisy_measurement = random.gauss(drone.z, sensor_sd)
     kf.update(measurement=noisy_measurement, measurement_variance=sensor_sd**2)
     
-    print(f"true_z={drone.z:.2f}, noisy_measurement={noisy_measurement:.2f}, kf_estimate={kf.estimate:.2f}")
+    print(f"true_z={drone.z:.2f}, noisy_measurement={noisy_measurement:.2f}, kf_estimate={kf.estimate:.2f}")'''
+
+
+
+
+drone = DroneState(0, 0, 0, 0, 0, 0, "AutopilotDrone")
+pid = PIDController(Kp=2, Ki=1, Kd=1, integral_limit=8)
+kf = KalmanFilter(estimate=0, variance=1, process_noise=0.01)
+
+target = 10
+m = 1
+g = 9.8
+sensor_sd = 0.5
+dt = 0.01
+
+for step in range(2000):
+    # 1. SENSE: noisy measurement of drone.z
+    noisy_measurement = random.gauss(drone.z, sensor_sd)
+    # 2. ESTIMATE: kf.predict(...) using drone.vz, then kf.update(...) with the noisy measurement
+    kf.predict(velocity=drone.vz, dt=dt)
+    kf.update(measurement=noisy_measurement, measurement_variance=sensor_sd**2)
+    # 3. DECIDE: error = target - kf.estimate (NOT drone.z!), then pid.compute(...)
+    error = target - kf.estimate
+    command = pid.compute(error, dt)
+
+    # 4. Convert command to az
+    az= g + command/m
+    # 5. ACT: drone.update(...)
+    drone.update(ax=0, ay=0, az=az, dt=dt)
+    
+    if step % 100 == 0:
+        print(f"true_z={drone.z:.2f}, kf_estimate={kf.estimate:.2f}, command={command}")
